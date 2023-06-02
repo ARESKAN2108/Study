c2cd3367fec1d548dc6e1134eaa20bc7cc41ff1c
ARESKAN2108 <maxareskan@gmail.com>
2023-06-02 21:34:38 +0300

    lesson 5

--- Lesson5/src/main/java/HomeWork.java
+++ Lesson5/src/main/java/HomeWork.java
@@ -1,6 +1,20 @@
 public class HomeWork {
     public static void main(String[] args) {
-        System.out.println(summ(0, 5));
+//        System.out.println(summ(0, 5));
+        System.out.println(summRec(6, -5));
+    }
+
+    private static int summRec(int a, int b) {
+        if (b == 0 || a == 0) {
+            return 0;
+        }
+        if (b < 0 && a < 0) {
+            a = -a;
+            b = -b;
+        } else if (b < 0) {
+            return b + summRec(b, --a);
+        }
+        return a + summRec(a, b < 0 ? ++b : --b);
     }
 
     private static int summ(int a, int b) {
