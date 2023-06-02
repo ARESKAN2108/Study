59a3e100b8956d8aa75443449e39b43ee3a54e05
ARESKAN2108 <maxareskan@gmail.com>
2023-06-02 18:56:45 +0300

    lesson 5

--- Lesson5/src/main/java/HomeWork.java
+++ Lesson5/src/main/java/HomeWork.java
@@ -1,2 +1,20 @@
 public class HomeWork {
+    public static void main(String[] args) {
+        System.out.println(summ(0, 5));
+    }
+
+    private static int summ(int a, int b) {
+        int result = 0;
+        if (a != 0 || b != 0) {
+            int absA = Math.abs(a);
+            int absB = Math.abs(b);
+            for (int i = 0; i < absB; i++) {
+                result += absA;
+            }
+        }
+        if (a > 0 ^ b > 0) {
+            result = -result;
+        }
+        return result;
+    }
 }
