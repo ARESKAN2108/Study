385dc437c7adba1d3c581f56f8d00161ccf29212
ARESKAN2108 <maxareskan@gmail.com>
2023-06-02 22:23:37 +0300

    lesson 5

--- Lesson1/src/Main.java
+++ Lesson1/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(Arrays.toString(reverse(5)));
+    }
+
+    public static int[] reverse(int n) {
+        int[] mass = new int[n];
+        for (int i = 0; i < mass.length; i++) {
+            mass[i] = n;
+            n--;
+        }
+        return mass;
     }
 }
